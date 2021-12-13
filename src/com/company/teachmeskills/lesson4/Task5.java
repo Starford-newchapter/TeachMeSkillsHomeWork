71726269688593d94940f2aaa0f17214e5b7a858
ua.karnilchyk <v.kornilchik2015@gmail.com>
2021-12-13 15:55:28 +0300

    Moving Task1 and Task 2 into one package

--- src/com/company/teachmeskills/lesson4/Task5.java
+++ src/com/company/teachmeskills/lesson4/Task5.java
@@ -1,12 +1,11 @@
-package com.company.teachmeskills;
+package com.company.teachmeskills.lesson4;
 
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
 
 
-public class Main {
-
+public class Task5 {
 
     private static int[][] generateArray() {
         System.out.println("Введите размерность массива ");
@@ -40,34 +39,27 @@ public class Main {
 
 
     public static void main(String[] args) {
-        countSumEvenElementsMainDiagonal();
+        transportMatrix();
+
 
     }
 
-    private static void countSumEvenElementsMainDiagonal() {
+    //Учтем заранее что матрица квадратная
+    public static void transportMatrix() {
         int[][] array = generateArray();
         printArray(array);
-        int sum = 0;
+
+        int temp;
 
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (i == j) {
-                    if (array[i][j] % 2 == 0) {
-                        sum += array[i][j];
-                    }
-                }
-
+            for (int j = i; j < array[i].length; j++) {
+                temp = array[i][j];
+                array[i][j] = array[j][i];
+                array[j][i] = temp;
             }
 
         }
 
-        System.out.println(sum);
-
-
+        printArray(array);
     }
-
-
 }
-
-
-
